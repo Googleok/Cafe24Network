5cc4d44084bd68ea0c0f428a8008636584769ed6
BIT <qkrwhddjr3@gmail.com>
2019-04-26 17:03:03 +0900

    Localhost 수정

--- src/test/Localhost.java
+++ src/test/Localhost.java
@@ -1,7 +1,13 @@
 package test;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Enumeration;
 
 public class Localhost {
 
@@ -17,19 +23,40 @@ public class Localhost {
 			
 			InetAddress[] inetAddresses = InetAddress.getAllByName(hostname);
 			
-//			int i = 0;
-//			for(InetAddress addr : inetAddresses) {
-//				System.out.println(i++);
-//				System.out.println(addr.getHostAddress());
-//			}
+			int i = 0;
+			for(InetAddress addr : inetAddresses) {
+				System.out.println(i++);
+				System.out.println(addr.getHostAddress());
+			}
 			
+			System.out.println("===================================");
 			
 			byte[] addresses = inetAddress.getAddress();
 			for( byte address : addresses) {
 				System.out.print(address & 0x000000ff);
 				System.out.print(".");
 			}
+			System.out.println();
+			System.out.println("===================================");
+			
+			NetworkInterface nif = NetworkInterface.getByName("eth0");
+			Enumeration<InetAddress> nifAddresses = nif.getInetAddresses();
+			System.out.println(nifAddresses.nextElement());
+			
+			for(NetworkInterface ifc : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+				if( ifc.isUp() == true &&
+					ifc.isVirtual() == false &&
+					ifc.isLoopback() == false) {
+					for(InetAddress inetAddr : Collections.list(ifc.getInetAddresses())) {
+						if(inetAddr.isLinkLocalAddress() == false) {
+							System.out.println(inetAddr.getHostAddress());
+						}
+					}
+				}
+			}
 		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
